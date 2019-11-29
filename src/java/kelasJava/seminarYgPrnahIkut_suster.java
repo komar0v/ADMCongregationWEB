5fa86aee93cfe2e948258bbf142129a152db5dcd
ASUS <ASUS@Diaz>
2019-11-29 15:45:13 +0700

    project untuk susteran ADM

---
+++ src/java/kelasJava/seminarYgPrnahIkut_suster.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelasJava;
+
+/**
+ *
+ * @author ASUS
+ */
+public class seminarYgPrnahIkut_suster extends biodata_suster{
+    String id_suster;
+    private String nama_seminar, nama_sertif_seminar;
+
+    public String getId_suster() {
+        return id_suster;
+    }
+
+    public void setId_suster(String id_suster) {
+        this.id_suster = id_suster;
+    }
+
+    public String getNama_seminar() {
+        return nama_seminar;
+    }
+
+    public void setNama_seminar(String nama_seminar) {
+        this.nama_seminar = nama_seminar;
+    }
+
+    public String getNama_sertif_seminar() {
+        return nama_sertif_seminar;
+    }
+
+    public void setNama_sertif_seminar(String nama_sertif_seminar) {
+        this.nama_sertif_seminar = nama_sertif_seminar;
+    }
+    
+    
+    
+}
